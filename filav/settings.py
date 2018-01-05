9ea06066c734f92575ca5447f836082a88f512f6
Juan Sebastian Goldberg <sebas.goldberg@gmail.com>
2018-01-05 08:56:21 -0300

    Implementada autenticação LDAP.

--- filav/settings.py
+++ filav/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels',
+
     'rest_framework',
     'social_django',
+    'django_python3_ldap',
+
     'fila',
 ]
 
@@ -169,6 +172,7 @@ AUTHENTICATION_BACKENDS = (
     #'social_core.backends.google.GoogleOAuth2',
     #'social_core.backends.google.GoogleOAuth',
     #'social_core.backends.yahoo.YahooOpenId',
+    'django_python3_ldap.auth.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
@@ -177,3 +181,92 @@ SOCIAL_AUTH_FACEBOOK_SECRET = os.getenv('SOCIAL_AUTH_FACEBOOK_SECRET')
 
 LOGIN_REDIRECT_URL = 'cliente'
 
+"""
+### LDAP CONGIG BEGIN
+"""
+
+# The URL of the LDAP server.
+LDAP_AUTH_URL = "ldap://localhost:3890"
+
+# Initiate TLS on connection.
+LDAP_AUTH_USE_TLS = False
+
+
+# The LDAP search base for looking up users.
+LDAP_AUTH_SEARCH_BASE = "dc=cencosud,dc=corp"
+
+# The LDAP class that represents a user.
+LDAP_AUTH_OBJECT_CLASS = "user"   # @todo
+
+# User model fields mapped to the LDAP
+# attributes that represent them.
+LDAP_AUTH_USER_FIELDS = {
+    "username": "sAMAccountName",
+    #"username": "uid",
+    "first_name": "displayName",
+    #"last_name": "sn",
+    "email": "mail",
+}
+
+# A tuple of django model fields used to uniquely identify a user.
+LDAP_AUTH_USER_LOOKUP_FIELDS = ("username",)
+
+# Path to a callable that takes a dict of {model_field_name: value},
+# returning a dict of clean model data.
+# Use this to customize how data loaded from LDAP is saved to the User model.
+#LDAP_AUTH_CLEAN_USER_DATA = "django_python3_ldap.utils.clean_user_data"
+
+# Path to a callable that takes a user model and a dict of {ldap_field_name: [value]},
+# and saves any additional user relationships based on the LDAP data.
+# Use this to customize how data loaded from LDAP is saved to User model relations.
+# For customizing non-related User model fields, use LDAP_AUTH_CLEAN_USER_DATA.
+#LDAP_AUTH_SYNC_USER_RELATIONS = "django_python3_ldap.utils.sync_user_relations"
+
+# Path to a callable that takes a dict of {ldap_field_name: value},
+# returning a list of [ldap_search_filter]. The search filters will then be AND'd
+# together when creating the final search filter.
+LDAP_AUTH_FORMAT_SEARCH_FILTERS = "django_python3_ldap.utils.format_search_filters"
+
+# Path to a callable that takes a dict of {model_field_name: value}, and returns
+# a string of the username to bind to the LDAP server.
+# Use this to support different types of LDAP server.
+#LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_openldap"
+#LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_active_directory"
+
+LDAP_AUTH_FORMAT_USERNAME = "django_python3_ldap.utils.format_username_active_directory_principal"
+LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = "cencosud.corp"
+
+# Sets the login domain for Active Directory users.
+#LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = 'ECDC'
+
+# The LDAP username and password of a user for querying the LDAP database for user
+# details. If None, then the authenticated user will be used for querying, and
+# the `ldap_sync_users` command will perform an anonymous query.
+LDAP_AUTH_CONNECTION_USERNAME = None
+LDAP_AUTH_CONNECTION_PASSWORD = None
+
+# Set connection/receive timeouts (in seconds) on the underlying `ldap3` library.
+LDAP_AUTH_CONNECT_TIMEOUT = None
+LDAP_AUTH_RECEIVE_TIMEOUT = None
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django_python3_ldap": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
+
+
+"""
+### LDAP CONGIG END
+"""
